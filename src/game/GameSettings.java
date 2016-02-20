c4e285ec1e1bdae15b834894cd2e065d05ff0474
czaloj <czaloj@gmail.com>
2016-02-20 09:42:07 -0500

    Move display mode enumeration to static call

--- src/game/GameSettings.java
+++ src/game/GameSettings.java
@@ -5,6 +5,7 @@ import org.lwjgl.opengl.DisplayMode;
 
 import java.lang.reflect.Array;
 import java.lang.reflect.Executable;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 
@@ -12,6 +13,41 @@ import java.util.Comparator;
  * \brief A struct of various application settings
  */
 public class GameSettings {
+    public static ArrayList<DisplayMode> availableDisplayModes = new ArrayList<>();
+
+    static {
+        // Grab a list of display modes sorted by size
+        DisplayMode[] modes = null;
+        try {
+            modes = Display.getAvailableDisplayModes();
+        }
+        catch (Exception e) {
+            System.err.println("Error in getting display modes");
+            System.err.println(e.getMessage());
+        }
+        Arrays.sort(modes, (DisplayMode o1, DisplayMode o2) -> {
+            int cmp = Integer.compare(o1.getWidth(), o2.getWidth());
+            if (cmp != 0) return cmp;
+            return Integer.compare(o1.getHeight(), o2.getHeight());
+        });
+
+        // Add all possible unique display modes
+        DisplayMode lastMode = null;
+        for (DisplayMode mode : modes) {
+            // TODO: We only care about fullscreen modes?
+            if (!mode.isFullscreenCapable()) continue;
+
+            // Make sure the mode is unique in size
+            if (lastMode != null) {
+                if (lastMode.getWidth() == mode.getWidth() && lastMode.getHeight() == mode.getHeight() && lastMode.getFrequency() == mode.getFrequency()) continue;
+            }
+
+            // Add unique mode to the list
+            availableDisplayModes.add(mode);
+            lastMode = mode;
+        }
+    }
+
     public static enum Preset {
         DEFAULT,
         LOW,
@@ -57,40 +93,11 @@ public class GameSettings {
         // TODO: Load settings from file if it exists
     }
     public void setToPreset(Preset p) {
-        DisplayMode[] modes = null;
-        try {
-            modes = Display.getAvailableDisplayModes();
-        }
-        catch (Exception e) {
-            System.err.println("Error in getting display modes");
-            System.err.println(e.getMessage());
-        }
-        Arrays.sort(modes, new Comparator<DisplayMode>() {
-            @Override
-            public int compare(DisplayMode o1, DisplayMode o2) {
-                int cmp = Integer.compare(o1.getWidth(), o2.getWidth());
-                if (cmp != 0) return cmp;
-                return Integer.compare(o1.getHeight(), o2.getHeight());
-            }
-        });
-        int availableCount = 0;
-        DisplayMode availableModes[] = new DisplayMode[modes.length];
-        for (DisplayMode mode : modes) {
-            // TODO: We only care about fullscreen modes?
-            if (!mode.isFullscreenCapable()) continue;
+        // TODO: Display modes should only be enumerated once
 
-            // Make sure the mode is unique in size
-            if (availableCount > 0) {
-                DisplayMode lastMode = availableModes[availableCount - 1];
-                if (lastMode.getWidth() == mode.getWidth() && lastMode.getHeight() == mode.getHeight() && lastMode.getFrequency() == mode.getFrequency()) continue;
-            }
-
-            // Add unique mode to the list
-            availableModes[availableCount++] = mode;
-        }
 
         // Figure out certain modes
-        DisplayMode highestMode = availableModes[availableCount - 1];
+        DisplayMode highestMode = availableDisplayModes.get(availableDisplayModes.size() - 1);
         resolutionWidth = highestMode.getWidth();
         resolutionHeight = highestMode.getHeight();
 
@@ -101,8 +108,19 @@ public class GameSettings {
                 useModernOpenGL = true;
                 physicsVelocityIterations = 4;
                 physicsPositionIterations = 4;
-            // TODO: Finish settings to a special preset
-
+                break;
+            case LOW:
+                // TODO: Finish settings to special preset
+                break;
+            case MEDIUM:
+                // TODO: Finish settings to special preset
+                break;
+            case HIGH:
+                // TODO: Finish settings to special preset
+                break;
+            case MAMMOTH:
+                // TODO: Finish settings to special preset
+                break;
         }
     }
     public void saveToFile() {
