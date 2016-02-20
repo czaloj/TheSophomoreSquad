e0fce086ba3092dcca8f436c8f09d31b2e440870
czaloj <czaloj@gmail.com>
2016-02-20 03:13:54 -0500

    Combo interface

---
+++ src/game/logic/ComboController.java
@@ -0,0 +1,19 @@
+package game.logic;
+
+import game.data.Combo;
+import game.data.EntityInput;
+
+public class ComboController {
+
+    public void addCombo() {
+
+    }
+
+    public void updateInput(EntityInput input, float dt) {
+
+    }
+
+    public Combo retrieveNewComboAction() {
+        return null;
+    }
+}
