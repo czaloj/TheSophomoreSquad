e36c13842e194e818d0a59b55ec111fcd0750d1c
czaloj <czaloj@gmail.com>
2016-02-22 23:47:11 -0500

    Mesh geometry struct

---
+++ src/game/graphics/MeshGeometry.java
@@ -0,0 +1,10 @@
+package game.graphics;
+
+/**
+ * Simple struct holds common mesh data elements
+ */
+public class MeshGeometry {
+    public int vertexDeclaration = 0;
+    public int vertexData = 0;
+    public int indexData = 0;
+}
