13fe6e562690bd5be08a3c265a422687a6142097
czaloj <czaloj@gmail.com>
2016-02-22 23:41:51 -0500

    Simple rendering skeleton

---
+++ src/game/graphics/DebugRenderer.java
@@ -0,0 +1,30 @@
+package game.graphics;
+
+import game.data.GameState;
+
+/**
+ * Renders the game as simple as possible
+ */
+public class DebugRenderer {
+    public DebugRenderer() {
+        // Empty
+    }
+
+
+    public void init() {
+
+    }
+    public void dispose() {
+
+    }
+
+    public void setState(GameState state) {
+
+    }
+
+
+    public void draw() {
+
+    }
+
+}
