bb6bd7e9b01f3775a0dec00f188e6c33765d26ef
czaloj <czaloj@gmail.com>
2016-02-25 00:57:33 -0500

    Camera information

--- src/game/data/GameState.java
+++ src/game/data/GameState.java
@@ -1,7 +1,20 @@
 package game.data;
 
+import egl.math.Vector2;
 import org.jbox2d.dynamics.World;
 
 public class GameState {
+    /**
+     * Contains all the physics information
+     */
     public World physicsWorld;
+
+    /**
+     * The center point of where the camera is looking
+     */
+    public final Vector2 cameraCenter = new Vector2();
+    /**
+     * The half-size of the bounds that the camera views
+     */
+    public final Vector2 cameraHalfViewSize = new Vector2();
 }
