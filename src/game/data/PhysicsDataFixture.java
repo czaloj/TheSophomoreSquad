e47368f6c9db86d58aad77ad9916a6f60fc4b705
czaloj <czaloj@gmail.com>
2016-03-05 15:51:47 -0500

    All the wasted time on my hands turns to sand

--- src/game/data/PhysicsDataFixture.java
+++ src/game/data/PhysicsDataFixture.java
@@ -4,5 +4,9 @@ package game.data;
  * This data is attached to all Box2D fixtures
  */
 public class PhysicsDataFixture {
+    public static final int OBJECT_TYPE_MAP = 0;
+    public static final int OBJECT_TYPE_CHARACTER = 1;
 
+    public int objectType;
+    public Object object;
 }
