49c268fa1bb0e1afc3448c68c10db80324542bbc
czaloj <czaloj@gmail.com>
2016-03-23 14:55:28 -0400

    Pretty blank rain demo

---
+++ src/presentation/RainScreen.java
@@ -0,0 +1,22 @@
+package presentation;
+
+import blister.GameTime;
+
+/**
+ *
+ */
+public class RainScreen extends SlideScreen {
+    @Override
+    public void update(GameTime gameTime) {
+        super.update(gameTime);
+
+        //
+    }
+
+    @Override
+    public void draw(GameTime gameTime) {
+
+
+        super.draw(gameTime);
+    }
+}
