c5bbe76d5c4c5acce5b81e8bba4823e0c94fd943
czaloj <czaloj@gmail.com>
2016-03-23 14:54:21 -0400

    Test slide

---
+++ src/presentation/PresentApp.java
@@ -0,0 +1,36 @@
+package presentation;
+
+import blister.MainGame;
+import blister.ScreenList;
+
+/**
+ * Created by czalo_000 on 3/21/2016.
+ */
+public class PresentApp extends MainGame {
+    public PresentApp() {
+        super("Tech Art", 1920, 1080);
+    }
+
+    @Override
+    protected void buildScreenList() {
+        screenList = new ScreenList(this, 0,
+            new ImageSlideScreen("data/slides/Hi.png")
+            );
+    }
+
+    @Override
+    protected void fullInitialize() {
+
+    }
+
+    @Override
+    protected void fullLoad() {
+
+    }
+
+    public static void main(String[] args) {
+        PresentApp app = new PresentApp();
+        app.run();
+        app.dispose();
+    }
+}
