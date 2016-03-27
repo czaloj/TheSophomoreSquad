589e900bc13be31b012f402ca1961d5fdcf28f09
czaloj <czaloj@gmail.com>
2016-03-26 23:11:58 -0400

    Go Fullscreen

--- src/presentation/PresentApp.java
+++ src/presentation/PresentApp.java
@@ -2,6 +2,8 @@ package presentation;
 
 import blister.MainGame;
 import blister.ScreenList;
+import game.GameSettings;
+import org.lwjgl.opengl.Display;
 
 /**
  * Created by czalo_000 on 3/21/2016.
@@ -26,7 +28,18 @@ public class PresentApp extends MainGame {
 
     @Override
     protected void fullLoad() {
+        // We'll check out what we can find at the beginning of the OpenGL context
+        GameSettings.global.queryCapabilities();
 
+        try {
+            // TODO: Enable this later
+            Display.setDisplayMode(GameSettings.availableDisplayModes.get(GameSettings.availableDisplayModes.size() - 1));
+            Display.setFullscreen(true);
+        }
+        catch (Exception e) {
+            System.err.println("Can't change the display mode");
+            System.err.println(e.getMessage());
+        }
     }
 
     public static void main(String[] args) {
