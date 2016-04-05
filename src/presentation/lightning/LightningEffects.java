58e8037e377fa7c6d654baf01948ee7eb0eae2b6
czaloj <czaloj@gmail.com>
2016-04-05 14:44:28 -0400

    Change default lightning parameters

--- src/presentation/lightning/LightningEffects.java
+++ src/presentation/lightning/LightningEffects.java
@@ -4,15 +4,15 @@ package presentation.lightning;
  *
  */
 public class LightningEffects {
-    public float fadePerFrame = 0.985f;
+    public float fadePerFrame = 0.9f;
 
-    public float boltWidth = 3.0f;
-    public float boltJag = 4.0f;
+    public float boltWidth = 2.0f;
+    public float boltJag = 8.0f;
     public float boltMin = 0.7f;
-    public float boltMax = 5.0f;
+    public float boltMax = 10.0f;
 
-    public int maxBoltsPerFrame = 400;
+    public int maxBoltsPerFrame = 5000;
     public int neighborChecks = 160;
-    public float minNeighborDistance = 7.0f;
+    public float minNeighborDistance = 8.0f;
     public float maxNeighborDistance = 300.0f;
 }
